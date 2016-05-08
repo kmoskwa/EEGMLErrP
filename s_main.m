acd8d4454eb5251a5252becc15e8ce6d76acbee7
Krzysztof Moskwa <krzysztof.moskwa@gmail.com>
2016-05-08 21:12:53 +0200

    New processing flags

--- s_main.m
+++ s_main.m
@@ -2,12 +2,12 @@ if ~exist('automation', 'var')
     clear;
     postFix           = '';
 end
-subjectInitials   = 'RZ';
 subjectInitials   = 'PG';
+subjectInitials   = 'RZ';
 sessionNo         = 1;
 drawDifferences   = 1; % to draw additional Error - Correct line (black) - set to 1
-refSubstract       = 1;
-limitStimNumber    = 1;
+refSubstract      = 1;
+limitStimNumber   = 1;
 % initials of operator
 windowBefore      = 1.0;  % s
 windowAfter       = 1.0;  % s
@@ -21,8 +21,10 @@ electrodes        = ['Cz '];
 electrodes        = ['Fz ';'Cz ';'C3 ';'C4 ';'Pz '];
 electrodes        = 'all';
 electrodes        = ['Fp1';'F3 ';'C3 ';'P3 ';'O1 ';'F7 ';'T3 ';'T5 ';'Fz ';'Fp2';'F4 ';'C4 ';'P4 ';'O2 ';'F8 ';'T4 ';'T6 ';'Cz ';'Pz '];
+electrodes        = ['Cz ';'F3 ';'F4 '];
 s_init();
 %stimulationsToProcess = [SA_STIM_WRONG SA_STIM_ENF_WRONG];
+%stimulationsToProcess = [SA_STIM_APPEAR SA_STIM_DISAPPEAR SA_STIM_OK SA_STIM_WRONG SA_STIM_ENF_WRONG SA_STIM_KB_MALFUNC];
 stimulationsToProcess = [SA_STIM_OK SA_STIM_WRONG SA_STIM_ENF_WRONG SA_STIM_KB_MALFUNC];
 stimulationsToProcess = SA_STIM_OK;
 stimulationsToProcess = [SA_STIM_OK SA_STIM_WRONG];
@@ -30,7 +32,6 @@ stimulationsToProcess = SA_STIM_ENF_WRONG;
 stimulationsToProcess = SA_STIM_WRONG;
 stimulationsToProcess = [SA_STIM_ENF_WRONG SA_STIM_WRONG];
 stimulationsToProcess = [SA_STIM_OK SA_STIM_WRONG SA_STIM_ENF_WRONG];
-%stimulationsToProcess = [SA_STIM_APPEAR SA_STIM_DISAPPEAR SA_STIM_OK SA_STIM_WRONG SA_STIM_ENF_WRONG SA_STIM_KB_MALFUNC];
 s_load();
 s_postInit();
 s_processStim();
